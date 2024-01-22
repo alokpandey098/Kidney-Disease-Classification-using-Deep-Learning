8598edf919881f8862098ba9920e149498d05c72
alokpandey098 <kalok0575@gmail.com>
2024-01-22 21:33:58 +0530

    Data ingestion

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-from cnnClassifier import logger
+from KidneyClassification import logger
+from KidneyClassification.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
